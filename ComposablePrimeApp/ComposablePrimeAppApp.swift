6db72322443de81eae51760217c380cdc6c94245
Pann Cherry <maung.eainmatmay@gmail.com>
2023-01-29 20:06:03 -0500

    Initial Commit

---
+++ ComposablePrimeApp/ComposablePrimeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposablePrimeAppApp.swift
+//  ComposablePrimeApp
+//
+//  Created by Pann Cherry on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ComposablePrimeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
