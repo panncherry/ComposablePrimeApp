1835494d18ba7b004a9e7b75c237e0f51850fee5
Pann Cherry <maung.eainmatmay@gmail.com>
2023-02-21 17:41:43 -0500

    Update AppReducer

--- ComposablePrimeApp/AppReducer.swift
+++ ComposablePrimeApp/AppReducer.swift
@@ -7,16 +7,8 @@
 
 import SwiftUI
 import Counter
-import PrimeModal
 import FavoritePrimes
 import StoreArchitecture
-
-//let _appReducer: Reducer<AppState, AppAction> = combine(pullback(counterReducer, value: \.count, action: \.counter),
-//                                                        pullback(primeModalReducer, value: \.primeModal, action: \.primeModal),
-//                                                        pullback(favoritePrimesReducer, value: \.favoritePrimes, action: \.favoritePrimes))
-//
-//
-//let appReducer = pullback(_appReducer, value: \.self, action: \.self)
 
 let appReducer = combine(
   pullback(counterViewReducer, value: \AppState.counterView, action: \AppAction.counterView),
